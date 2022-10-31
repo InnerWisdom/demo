a53d2216c582e9711b44f5f68c8eea83b7e6f2a5
InnerWisdom <68237197+InnerWisdom@users.noreply.github.com>
2022-10-31 08:55:31 +0400

    Initial commit

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+
+public class Controller {
+    public String par1() {
+        return "Hello World!";
+    }
+
+    public String par2() {
+        return "123";
+    }
+
+    public String par3() {
+        return "No?!";
+    }
+}
---
+++ src/test/java/com/example/demo/tests.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class tests {
+
+    @Test
+    public void testHomeController() {
+        Controller homeController = new Controller();
+        String result = homeController.par1();
+        assertEquals(result, "Hello World!");
+    }
+    @Test
+    public void testHomeController2() {
+        Controller homeController = new Controller();
+        String result = homeController.par2();
+        assertEquals(result, "123");
+    }
+    @Test
+    public void testHomeController3() {
+        Controller homeController = new Controller();
+        String result = homeController.par3();
+        assertEquals(result, "No?");
+    }
+
+}
